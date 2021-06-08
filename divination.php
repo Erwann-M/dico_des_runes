6018a844f9c2d2a5fe013c0dedb4b968fce23342
Nnawre <rwnnmrtn@gmail.com>
2021-06-08 18:08:52 +0200

    debut responsive, affichage dynamique titre d'onglet

--- divination.php
+++ divination.php
@@ -3,7 +3,15 @@
 <body>
     <div id="corp">
         <?php require "encadrement.php"; ?>
-        <img src="img/construction.png">
+        <h1>Bienvenu sur le jeu de divination</h1>
+        <p>Si j'ai choisie d'appeler cette partie : "jeu de divination" c'est principalement pour vous signifier qu'il ne faut pas prendre de decision lourde en fonction des reponses que vous trouverez sur cette page, prenez le plutot comme un conseil que comme la vérité absolue conscernant votre avenir.<br> 
+        Biensur chacun est libre de croire ce qu'il veut.</p>
+
+        <div id="choix_tirage">
+            
+        
+        </div>
+
     </div>
 </body>
 
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/png" href="img/arbre-icon.png" size="16x16">
-    <title>Dico des runes</title>
+    <title>Dico des runes<?php if (isset($_GET['page'])) {echo " | " . $_GET['page'];} ?></title>
 </head>
 
 <header>
@@ -18,10 +18,10 @@
         <ul id="nav-list">
             <img id="icon-nav" src="img/arbre-icon2.png">
             <li><a href="index.php">Acceuil</a></li>
-            <li><a href="talisman.php">Creation de talisman</a></li>
-            <li><a href="divination.php">Jeu de divination</a></li>
-            <li><a href="dieux.php">Mythologie nordique</a></li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="talisman.php?page=Talisman">Creation de talisman</a></li>
+            <li><a href="divination.php?page=Divination">Jeu de divination</a></li>
+            <li><a href="dieux.php?page=Mythologie">Mythologie nordique</a></li>
+            <li><a href="contact.php?page=Contact">Contact</a></li>
         </ul>
     </div>
 </header>
